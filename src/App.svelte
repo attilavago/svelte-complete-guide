80c79ec3c6a4aabfce30d05c1a22b6a59c1e91df
Attila Vago <attilavago85@gmail.com>
2019-05-24 20:40:23 +0100

    done with lesson 6

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Attila';
+	let age = 33;
+
+	function incrementAge(){
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, you are {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
